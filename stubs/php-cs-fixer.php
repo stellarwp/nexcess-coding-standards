9bfac42993997698bdccca9d0f7de564701388ec
Steve Grunwell <sgrunwell@liquidweb.com>
2022-02-04 15:10:31 -0500

    Establish our default coding standards for StellarWP packages
    The standard is based on PSR-12, with some WordPress-specific tools added for inline documentation and general best-practices.

---
+++ stubs/php-cs-fixer.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Custom configuration for PHP-CS-Fixer.
+ *
+ * @link https://cs.symfony.com/
+ */
+
+/*
+ * First, retrieve the default configuration object.
+ */
+$config = require_once __DIR__ . '/vendor/stellarwp/coding-standards/src/php-cs-fixer.php';
+
+/*
+ * Want to override the files being checked? Modify the Finder instance:
+ *
+ *     $config->getFinder()
+ *         ->path('my-plugin.php');
+ *
+ * @link https://symfony.com/doc/current/components/finder.html
+ */
+
+/*
+ * Need to adjust the rules? Merge your overrides with the defaults!
+ *
+ *     $config->setRules(array_merge($config->getRules(), [
+ *         // Your rules go here.
+ *     ]));
+ *
+ * @link https://cs.symfony.com/doc/rules/index.html
+ */
+
+/*
+ * IMPORTANT: the $config object must be returned from this file!
+ */
+return $config;
